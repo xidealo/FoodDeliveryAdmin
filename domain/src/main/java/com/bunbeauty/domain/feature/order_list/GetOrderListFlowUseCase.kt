a9ea33a5c32fcb312d0e77ecb8e8481a1d2c8240
v.podkopaev <max.astin@yandex.ru>
2023-06-30 22:54:51 +0300

    add canceled order filter

--- domain/src/main/java/com/bunbeauty/domain/feature/order_list/GetOrderListFlowUseCase.kt
+++ domain/src/main/java/com/bunbeauty/domain/feature/order_list/GetOrderListFlowUseCase.kt
@@ -1,11 +1,13 @@
 package com.bunbeauty.domain.feature.order_list
 
+import com.bunbeauty.domain.enums.OrderStatus
 import com.bunbeauty.domain.model.order.Order
 import com.bunbeauty.domain.repo.DataStoreRepo
 import com.bunbeauty.domain.repo.OrderRepo
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetOrderListFlowUseCase @Inject constructor(
@@ -19,7 +21,11 @@ class GetOrderListFlowUseCase @Inject constructor(
         return orderRepository.getOrderListFlow(
             token = token,
             cafeUuid = cafeUuid,
-        )
+        ).map { orderList ->
+            orderList.filter {  order ->
+                order.orderStatus != OrderStatus.CANCELED
+            }
+        }
     }
 
 }
