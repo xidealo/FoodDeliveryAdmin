23723370f3ddfaaae739e69965b4b5c49345e932
v.podkopaev <max.astin@yandex.ru>
2023-08-02 23:25:03 +0300

    update imports

--- app/src/main/java/com/bunbeauty/fooddeliveryadmin/core_ui/BaseFragment.kt
+++ app/src/main/java/com/bunbeauty/fooddeliveryadmin/core_ui/BaseFragment.kt
@@ -17,7 +17,6 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.bunbeauty.fooddeliveryadmin.R
-import com.google.android.material.R as MR
 import com.bunbeauty.fooddeliveryadmin.util.startedLaunch
 import com.bunbeauty.presentation.view_model.BaseViewModel
 import com.google.android.material.snackbar.Snackbar
@@ -26,6 +25,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import java.lang.reflect.ParameterizedType
+import com.google.android.material.R as MR
 
 abstract class BaseFragment<B : ViewBinding> : Fragment() {
 
@@ -35,7 +35,6 @@ abstract class BaseFragment<B : ViewBinding> : Fragment() {
     protected val textInputMap = HashMap<String, TextInputLayout>()
     protected abstract val viewModel: BaseViewModel
 
-    @Suppress("UNCHECKED_CAST")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
