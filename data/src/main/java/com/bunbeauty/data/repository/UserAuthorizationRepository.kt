35b401fb68ca33f49a4b381c7f92a9ebea1765dc
xIdealo <shavl.mark@yandex.ru>
2023-10-30 23:51:01 +0300

    fix login

--- data/src/main/java/com/bunbeauty/data/repository/UserAuthorizationRepository.kt
+++ data/src/main/java/com/bunbeauty/data/repository/UserAuthorizationRepository.kt
@@ -3,7 +3,6 @@ package com.bunbeauty.data.repository
 import com.bunbeauty.common.ApiResult
 import com.bunbeauty.data.FoodDeliveryApi
 import com.bunbeauty.data.model.server.request.UserAuthorizationRequest
-import com.bunbeauty.domain.exception.LoginException
 import com.bunbeauty.domain.repo.UserAuthorizationRepo
 import javax.inject.Inject
 
@@ -14,13 +13,15 @@ class UserAuthorizationRepository @Inject constructor(
     override suspend fun login(
         username: String,
         password: String
-    ): Triple<String, String, String> {
-        return when (val result = networkConnector.login(
-            UserAuthorizationRequest(
-                username = username,
-                password = password
+    ): Triple<String, String, String>? {
+        return when (
+            val result = networkConnector.login(
+                UserAuthorizationRequest(
+                    username = username,
+                    password = password
+                )
             )
-        )) {
+        ) {
             is ApiResult.Success -> {
                 Triple(
                     result.data.token,
@@ -29,7 +30,7 @@ class UserAuthorizationRepository @Inject constructor(
                 )
             }
 
-            is ApiResult.Error -> throw LoginException()
+            is ApiResult.Error -> null
         }
     }
 
--- domain/src/main/java/com/bunbeauty/domain/repo/UserAuthorizationRepo.kt
+++ domain/src/main/java/com/bunbeauty/domain/repo/UserAuthorizationRepo.kt
@@ -1,5 +1,5 @@
 package com.bunbeauty.domain.repo
 
 interface UserAuthorizationRepo {
-    suspend fun login(username: String, password: String): Triple<String, String, String>
+    suspend fun login(username: String, password: String): Triple<String, String, String>?
 }
