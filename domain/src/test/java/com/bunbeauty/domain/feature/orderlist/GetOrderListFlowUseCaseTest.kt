dce1fb25a053eecfbba4bb8274658b4afb7306c1
v.podkopaev <max.astin@yandex.ru>
2024-11-08 12:19:50 +0200

    fix unit tests

--- domain/src/test/java/com/bunbeauty/domain/feature/orderlist/GetOrderListFlowUseCaseTest.kt
+++ domain/src/test/java/com/bunbeauty/domain/feature/orderlist/GetOrderListFlowUseCaseTest.kt
@@ -6,7 +6,6 @@ import com.bunbeauty.domain.model.order.Order
 import com.bunbeauty.domain.repo.DataStoreRepo
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
@@ -14,7 +13,6 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 
-@OptIn(ExperimentalCoroutinesApi::class)
 internal class GetOrderListFlowUseCaseTest {
 
     private val dataStoreRepo: DataStoreRepo = mockk()
@@ -34,7 +32,7 @@ internal class GetOrderListFlowUseCaseTest {
         // Given
         val token = "token"
         val cafeUuid = "cafeUuid"
-        coEvery { dataStoreRepo.token } returns flowOf(token)
+        coEvery { dataStoreRepo.getToken() } returns token
         coEvery { orderRepo.getOrderListFlow(token, cafeUuid) } returns flowOf(
             listOf(
                 createOrder(OrderStatus.NOT_ACCEPTED),
