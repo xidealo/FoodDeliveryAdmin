b44444aaec0143cb37b881ea369def3695f6bd2c
xIdealo <shavl.mark@yandex.ru>
2024-12-31 01:56:57 +0300

    fix worker

--- app/src/main/java/com/bunbeauty/fooddeliveryadmin/di/WorkManagerModule.kt
+++ app/src/main/java/com/bunbeauty/fooddeliveryadmin/di/WorkManagerModule.kt
@@ -1,10 +1,11 @@
 package com.bunbeauty.fooddeliveryadmin.di
 
 import com.bunbeauty.data.work.UpdateNotificationTokenWorker
+import org.koin.androidx.workmanager.dsl.worker
 import org.koin.dsl.module
 
 fun workManagerModule() = module {
-    single {
+    worker {
         UpdateNotificationTokenWorker(
             appContext = get(),
             workerParams = get(),
