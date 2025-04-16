74816c0a837d1fbdcfd2e064627ab1f7ad8e1251
Ivan1 <zhegalin98@yandex.ru>
2025-04-16 21:43:59 +0300

    fix EditCategoryFragment

--- presentation/src/main/java/com/bunbeauty/presentation/feature/category/editcategory/EditCategoryState.kt
+++ presentation/src/main/java/com/bunbeauty/presentation/feature/category/editcategory/EditCategoryState.kt
@@ -1,5 +1,6 @@
 package com.bunbeauty.presentation.feature.category.editcategory
 
+import com.bunbeauty.presentation.feature.menulist.common.TextFieldData
 import com.bunbeauty.presentation.viewmodel.base.BaseAction
 import com.bunbeauty.presentation.viewmodel.base.BaseDataState
 import com.bunbeauty.presentation.viewmodel.base.BaseEvent
@@ -7,12 +8,18 @@ import com.bunbeauty.presentation.viewmodel.base.BaseEvent
 interface EditCategoryState {
     data class DataState(
         val uuid: String,
-        val name: String,
+        val name: TextFieldData,
         val state: State,
         val isLoading: Boolean,
-        val hasEditNameError: Boolean,
-        val hasDuplicateNameError: Boolean
+        val nameStateError: NameStateError
     ) : BaseDataState {
+
+        enum class NameStateError {
+            EMPTY_NAME,
+            DUPLICATE_NAME,
+            NO_ERROR
+        }
+
         enum class State {
             LOADING,
             SUCCESS,
