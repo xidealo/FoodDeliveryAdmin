ff34378e415bb128d47095b19ceac81573b827a0
xIdealo <shavl.mark@yandex.ru>
2025-10-29 22:12:30 +0300

    fix UpdateCategory.kt

--- domain/src/main/java/com/bunbeauty/domain/feature/menu/common/model/UpdateCategory.kt
+++ domain/src/main/java/com/bunbeauty/domain/feature/menu/common/model/UpdateCategory.kt
@@ -2,12 +2,12 @@ package com.bunbeauty.domain.feature.menu.common.model
 
 data class UpdateCategory(
     val name: String,
-    val priority: Int
+    val priority: Int? = null
 ) {
     companion object {
         val mock = UpdateCategory(
             name = "",
-            priority = 0
+            priority = null
         )
     }
 }
