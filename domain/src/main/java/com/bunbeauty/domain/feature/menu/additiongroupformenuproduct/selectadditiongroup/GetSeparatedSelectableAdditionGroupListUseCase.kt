b2328bfb9c76dece2d74fa95ae4e895ba9782eb0
xIdealo <shavl.mark@yandex.ru>
2025-11-19 00:00:03 +0300

    added tests

--- domain/src/main/java/com/bunbeauty/domain/feature/menu/additiongroupformenuproduct/selectadditiongroup/GetSeparatedSelectableAdditionGroupListUseCase.kt
+++ domain/src/main/java/com/bunbeauty/domain/feature/menu/additiongroupformenuproduct/selectadditiongroup/GetSeparatedSelectableAdditionGroupListUseCase.kt
@@ -16,7 +16,6 @@ data class SelectableAdditionGroup(
     val isSelected: Boolean,
 )
 
-// TODO tests
 class GetSeparatedSelectableAdditionGroupListUseCase(
     private val getSeparatedAdditionGroupListUseCase: GetSeparatedAdditionGroupListUseCase,
     private val menuProductToAdditionGroupRepository: MenuProductToAdditionGroupRepository,
