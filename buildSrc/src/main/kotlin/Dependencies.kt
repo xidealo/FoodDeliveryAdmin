dfd4ee2981b44d1b3f229e237ab1219c1831e03e
xIdealo <shavl.mark@yandex.ru>
2025-12-09 00:51:45 +0300

    update versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,11 +1,11 @@
 object Application {
     const val versionMajor = 2
     const val versionMinor = 0
-    const val versionPatch = 4
+    const val versionPatch = 5
 
     const val versionName = "${versionMajor}.${versionMinor}.$versionPatch"
     const val applicationId = "com.bunbeauty.fooddeliveryadmin"
-    const val versionCode = 204
+    const val versionCode = 205
 }
 
 object Namespace {
