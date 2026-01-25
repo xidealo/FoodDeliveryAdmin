afe4206ccb189bc5bac53769aab937fd46177e5e
Ivan Zhegalin <111979803+DeputatDev@users.noreply.github.com>
2026-01-25 18:02:56 +0300

    add MultiplatformFeatureConventionPlugin (#131)
    Co-authored-by: deputat <zhegalinie98@yandex.ru>

---
+++ build-logic/convention/src/main/kotlin/com/bunbeauty/conventions/MultiplatformFeatureConventionPlugin.kt
@@ -0,0 +1,39 @@
+package com.bunbeauty.conventions
+
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
+import com.android.build.gradle.LibraryExtension
+import com.bunbeauty.AndroidSdk
+import com.bunbeauty.configureKotlinAndroid
+import com.bunbeauty.disableUnnecessaryAndroidTests
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
+class MultiplatformFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(receiver = target) {
+            with(receiver = pluginManager) {
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("com.android.library")
+                apply("org.jlleitschuh.gradle.ktlint")
+            }
+
+            extensions.configure<KotlinMultiplatformExtension> {
+                applyDefaultHierarchyTemplate()
+                androidTarget()
+                iosX64()
+                iosArm64()
+                iosSimulatorArm64()
+            }
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = AndroidSdk.TARGET
+            }
+            extensions.configure<LibraryAndroidComponentsExtension> {
+                disableUnnecessaryAndroidTests(target)
+            }
+
+        }
+    }
+}
