b5510527e109d34eaddd15fb0e61083d2dedcf5c
deputat <zhegalinie98@yandex.ru>
2026-01-26 00:46:24 +0300

    fix ktLintFormat

--- presentation/src/main/java/com/bunbeauty/presentation/feature/profile/Profile.kt
+++ presentation/src/main/java/com/bunbeauty/presentation/feature/profile/Profile.kt
@@ -32,6 +32,7 @@ interface Profile {
         data object StatisticClick : Action
 
         data object SettingsClick : Action
+
         data object LogoutClick : Action
 
         data object MapClick : Action
@@ -43,6 +44,7 @@ interface Profile {
 
     sealed interface Event : BaseEvent {
         data object OpenSettings : Event
+
         data object OpenStatistic : Event
 
         data object OpenLogin : Event
