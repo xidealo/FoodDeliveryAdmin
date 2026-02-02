9128b73ff54633d809a0c6e05a7b0c9da681583a
deputat <zhegalinie98@yandex.ru>
2026-02-03 00:47:11 +0300

    migration data module to kmp

--- data/src/iosMain/kotlin/com/bunbeauty/data/repository/PhotoRepository.ios.kt
+++ data/src/iosMain/kotlin/com/bunbeauty/data/repository/PhotoRepository.ios.kt
@@ -4,12 +4,15 @@ import com.bunbeauty.domain.model.Photo
 import com.bunbeauty.domain.repo.PhotoRepo
 
 actual class PhotoRepository : PhotoRepo {
+    private var photoListCache: List<Photo>? = null
+
     actual override suspend fun getPhotoList(username: String): List<Photo> {
-        TODO("Not yet implemented")
+        return photoListCache.orEmpty()
     }
 
     actual override suspend fun fetchPhotoList(username: String): List<Photo> {
-        TODO("Not yet implemented")
+        photoListCache = emptyList()
+        return emptyList()
     }
 
     actual override suspend fun uploadPhoto(
@@ -18,14 +21,14 @@ actual class PhotoRepository : PhotoRepo {
         width: Int,
         height: Int,
     ): Photo? {
-        TODO("Not yet implemented")
+        throw NotImplementedError("Photo upload is not implemented on iOS yet")
     }
 
     actual override suspend fun deletePhoto(photoLink: String) {
-        TODO("Not yet implemented")
+
     }
 
     actual override fun clearCache() {
-        TODO("Not yet implemented")
+        photoListCache == null
     }
 }
