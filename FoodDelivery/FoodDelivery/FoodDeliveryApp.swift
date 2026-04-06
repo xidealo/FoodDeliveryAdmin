65634c39715bd6db6276d551e79d8623155ab371
Mark.Shavlovsky <Mark.Shavlovsky@x5.ru>
2026-04-06 23:47:49 +0300

    start add ios

---
+++ FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Mark.Shavlovsky on 06.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
