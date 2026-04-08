0067680364819378986d041ad95b018a082e950f
Mark.Shavlovsky <Mark.Shavlovsky@x5.ru>
2026-04-08 23:56:18 +0300

    update аAppIos

--- FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
+++ FoodDelivery/FoodDelivery/FoodDeliveryApp.swift
@@ -5,13 +5,30 @@
 //  Created by Mark.Shavlovsky on 06.04.2026.
 //
 
+import shared
 import SwiftUI
 
 @main
 struct FoodDeliveryApp: App {
+    init() {
+        KoinKt.doInitKoin()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ComposeView()
+                .ignoresSafeArea()
         }
     }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let vc = AppIosKt.MainViewController()
+        vc.view.insetsLayoutMarginsFromSafeArea = false
+        vc.additionalSafeAreaInsets = .zero
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
