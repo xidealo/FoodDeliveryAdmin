ab84ae86843e8ee777cd3f03192b7bdaddd13441
deputat <zhegalinie98@yandex.ru>
2026-05-01 16:09:56 +0300

    delete import

--- shared/src/commonMain/kotlin/com/bunbeauty/shared/designsystem/compose/element/textfield/AdminBaseTextField.kt
+++ shared/src/commonMain/kotlin/com/bunbeauty/shared/designsystem/compose/element/textfield/AdminBaseTextField.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
