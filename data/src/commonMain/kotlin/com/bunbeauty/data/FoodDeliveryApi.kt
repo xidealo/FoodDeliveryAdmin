a55009895e3192121e7aa16105a07d1ed121fe9d
Mark Shavlovskij <36772847+xidealo@users.noreply.github.com>
2026-07-14 22:14:50 +0300

    Add GetClientUserStatisticUseCase for client details

--- data/src/commonMain/kotlin/com/bunbeauty/data/FoodDeliveryApi.kt
+++ data/src/commonMain/kotlin/com/bunbeauty/data/FoodDeliveryApi.kt
@@ -18,6 +18,7 @@ import com.bunbeauty.data.model.server.category.CreateCategoryPostServer
 import com.bunbeauty.data.model.server.category.PatchCategoryList
 import com.bunbeauty.data.model.server.city.CityServer
 import com.bunbeauty.data.model.server.clientuser.ClientUserSettingsServer
+import com.bunbeauty.data.model.server.clientuser.ClientUserStatisticServer
 import com.bunbeauty.data.model.server.company.CompanyPatchServer
 import com.bunbeauty.data.model.server.company.WorkInfoData
 import com.bunbeauty.data.model.server.menuProductToAdditionGroup.MenuProductToAdditionGroupServer
@@ -67,6 +68,11 @@ interface FoodDeliveryApi {
         limit: Int,
         offset: Int,
     ): ApiResult<ServerList<ClientUserSettingsServer>>
+
+    suspend fun getClientUserStatistic(
+        token: String,
+        clientUserUuid: String,
+    ): ApiResult<ClientUserStatisticServer>
 
     // CAFE
     suspend fun getCafeList(cityUuid: String): ApiResult<ServerList<CafeServer>>
